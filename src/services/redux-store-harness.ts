1a04b54b459255bea56a686e7ba9b28cac0e59c1
rain9441 <rain9441@gmail.com>
2020-09-27 19:51:19 -0500

    fix for recent react update store code

--- src/services/redux-store-harness.ts
+++ src/services/redux-store-harness.ts
@@ -2,21 +2,17 @@ class ReduxStoreHarness {
     GetStore() {
         var root: any = document.getElementById('container');
 
+        // document.getElementById('container')._reactRootContainer._internalRoot.current.child.child.child.pendingProps.store.getState().toJS()
         if (!root) {
             throw new Error("Unable to find container element, this may not be running on APEX.");
         }
-        root = root.children[0];
-        if (!root) {
-            throw new Error("Unable to find child of root container element, this may not be running on APEX.");
-        }
 
-        var reactPropertyName = Object.keys(root).filter(x => x.substring(0,5) == "__rea")[0];
-        if (!reactPropertyName)
-        {
-            throw new Error("Unable to find react instance property name, this may not be running on APEX.");
+        try {
+            var js = root._reactRootContainer._internalRoot.current.child.child.child.pendingProps.store.getState().toJS()
+            return js;
+        } catch (e) {
+            throw new Error("Unable to find state on container element.");
         }
-
-        return root[reactPropertyName]._currentElement._owner._context.store.getState().toJS();
     }; 
 }
 
